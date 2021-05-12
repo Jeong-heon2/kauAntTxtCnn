5badea975be788222d76d4f85e596a58e32068c6
Jeong-heon2 <cjhgg2@gmail.com>
2021-05-12 15:42:21 +0900

    slack 연동 확인

--- CNN/TextCnn.py
+++ CNN/TextCnn.py
@@ -313,6 +313,7 @@ with tf.device("/cpu:0"):
                 writer.add_summary(summaries, step)
 
 
+
         def batch_iter(data, batch_size, num_epochs, shuffle=True):
             """
             Generates a batch iterator for a dataset.
